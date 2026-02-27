0db78acff1d6ef050c5e6f4cd9c024cd13d01e2b
QuangTTSE182608 <quangttse182608@fpt.edu.vn>
2026-02-27 09:59:15 +0700

    Remove AccountId from Order entity and update migration
    This commit removes the `AccountId` property from the `Order` entity in `ApplicationDbContext.cs`, along with its foreign key relationship and navigation property. Corresponding updates were made in `ApplicationDbContextModelSnapshot.cs` to reflect these changes. A new migration, `fixOrderTable`, was created to drop the foreign key constraint, index, and `AccountId` column from the `Orders` table. The migration includes a `Down` method for potential restoration of the removed elements. These changes simplify the data model and its relationships.

---
+++ Infrastructure/Migrations/20260226043601_fixOrderTable.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantOrderTracking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixOrderTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Accounts_AccountId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_AccountId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "Orders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AccountId",
+                table: "Orders",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_AccountId",
+                table: "Orders",
+                column: "AccountId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Accounts_AccountId",
+                table: "Orders",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
