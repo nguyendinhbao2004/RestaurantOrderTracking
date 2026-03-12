1cdd434f3e27f10b3619544cfc7410d0043c9135
QuangTTSE182608 <quangttse182608@fpt.edu.vn>
2026-03-12 13:04:34 +0700

    fix order create
    xóa customerId

--- Application/Feature/Order/Commands/Create/CreateOrderHandler.cs
+++ Application/Feature/Order/Commands/Create/CreateOrderHandler.cs
@@ -1,4 +1,4 @@
-﻿using MediatR;
+using MediatR;
 using RestaurantOrderTracking.Domain.Common;
 using RestaurantOrderTracking.Domain.Enums;
 using RestaurantOrderTracking.Domain.Interface;
@@ -21,52 +21,24 @@ namespace RestaurantOrderTracking.Application.Feature.Order.Commands.Create
         }
         public async Task<Result<Guid>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
         {
-
             // ===============================
-            // 1️⃣ Validate theo OrderType
+            // 1️⃣ Validate: TableId là bắt buộc (DineIn / TakeAway)
             // ===============================
 
-            if (request.OrderType == OrderType.Delivery)
-            {
-                if (request.CustomerId == null)
-                    return Result<Guid>.Failure("CustomerId is required for Delivery order.");
+            if (request.TableId == null)
+                return Result<Guid>.Failure("TableId is required for DineIn or TakeAway order.");
 
-                if (request.TableId != null)
-                    return Result<Guid>.Failure("Delivery order cannot have TableId.");
-            }
-            else
-            {
-                if (request.TableId == null)
-                    return Result<Guid>.Failure("TableId is required for DineIn or TakeAway order.");
+            var hasActiveOrder = await _orderRepository
+                .TableHasActiveOrder(request.TableId.Value);
 
-                if (request.CustomerId != null)
-                    return Result<Guid>.Failure("DineIn/TakeAway order cannot have CustomerId.");
-            }
-
-
-
-            if (request.TableId.HasValue)
-            {
-                var hasActiveOrder = await _orderRepository
-                    .TableHasActiveOrder(request.TableId.Value);
-
-                if (hasActiveOrder)
-                    return Result<Guid>.Failure("This table already has an active order.");
-            }
-
+            if (hasActiveOrder)
+                return Result<Guid>.Failure("This table already has an active order.");
 
             var order = new OrderEntity(
-                tableId: request.TableId ?? Guid.Empty,  // nếu delivery thì tạm để Guid.Empty
+                tableId: request.TableId.Value,
                 orderType: request.OrderType,
                 waiterId: request.AccountId
             );
-
-            if (request.OrderType == OrderType.Delivery)
-            {
-                typeof(OrderEntity)
-                    .GetProperty("TableId")?
-                    .SetValue(order, null);
-            }
 
             await _orderRepository.AddAsync(order);
             await _unitOfWork.SaveChangesAsync(cancellationToken);
